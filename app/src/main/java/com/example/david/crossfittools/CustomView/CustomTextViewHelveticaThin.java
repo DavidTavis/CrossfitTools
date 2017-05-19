32e47c8c3fe1cc061fa1cb434137d87b926b098e
David Tarielashvili <david.tavis@gmail.com>
2017-05-19 16:01:00 +0300

    no message

--- app/src/main/java/com/example/david/crossfittools/CustomView/CustomTextViewHelveticaThin.java
+++ app/src/main/java/com/example/david/crossfittools/CustomView/CustomTextViewHelveticaThin.java
@@ -10,21 +10,21 @@ import android.util.AttributeSet;
  * Created by TechnoA on 16.05.2017.
  */
 
-public class CustomTextView extends android.support.v7.widget.AppCompatTextView {
+public class CustomTextViewHelveticaThin extends android.support.v7.widget.AppCompatTextView {
 
-    public CustomTextView(Context context) {
+    public CustomTextViewHelveticaThin(Context context) {
         super(context);
         Typeface face = Typeface.createFromAsset(context.getAssets(), "fonts/HelveticaThin.ttf");
         this.setTypeface(face);
     }
 
-    public CustomTextView(Context context, AttributeSet attrs) {
+    public CustomTextViewHelveticaThin(Context context, AttributeSet attrs) {
         super(context, attrs);
         Typeface face = Typeface.createFromAsset(context.getAssets(), "fonts/HelveticaThin.ttf");
         this.setTypeface(face);
     }
 
-    public CustomTextView(Context context, AttributeSet attrs, int defStyle) {
+    public CustomTextViewHelveticaThin(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         Typeface face = Typeface.createFromAsset(context.getAssets(), "fonts/HelveticaThin.ttf");
         this.setTypeface(face);
